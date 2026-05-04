fd58e31abbfca403a0d58457e479cde383f90305
ckal0417 <ckal17042004@gmail.com>
2026-05-03 22:03:06 -0500

    seagrega todo

--- Front/portal-practica1/src/app/app.ts
+++ Front/portal-practica1/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header-component/header-component';
+import { BodyComponent } from './body-component/body-component';
 import { FooterComponent } from './footer-component/footer-component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent
+  imports: [RouterOutlet, HeaderComponent, BodyComponent, FooterComponent
     
   ],
   templateUrl: './app.html',
