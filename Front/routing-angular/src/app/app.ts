93a59286646bacc92828d8740e21d001c4968769
ckal0417 <ckal17042004@gmail.com>
2026-05-13 12:33:48 -0500

    mejores de opciones completas en routlink

--- Front/routing-angular/src/app/app.ts
+++ Front/routing-angular/src/app/app.ts
@@ -2,13 +2,15 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponents } from './shared/header-components/header-components';
 import { FooterComponents } from './shared/footer-components/footer-components';
+import { NavbarComponents } from './shared/navbar-components/navbar-components';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
     HeaderComponents,
-    FooterComponents
+    FooterComponents,
+    NavbarComponents
   ],
 
   templateUrl: './app.html',
